5a3cb7bb69aec2f0c6944c2cee3a91caf6a4990f
John Ville Sanguyo <j.sanguyo@manteq-me.com>
2022-06-28 10:28:11 +0400

    Initial Commit

---
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace SimpleAPI.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ValuesController : ControllerBase
+{
+    [HttpGet]
+    public ActionResult<IEnumerable<string>> Get()
+    {
+        return new string[] { "value1", "value2" };
+
+    }
+
+    [HttpGet("{id}")]
+    public ActionResult<string> Get(int value)
+    {
+        return "John";
+    }
+}
